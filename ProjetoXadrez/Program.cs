e364493c878ec030e6f4ae369719a0b1a6d68074
Pedro Henrique <phsferreira06@gmail.com>
2025-04-07 23:21:46 -0300

    Classe posição criada

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tabuleiro;
+namespace ProjetoXadrez
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao = "+ p);
+        }
+    }
+
+
+
+}
+
